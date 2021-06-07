941ea7ff4f800a20cec544e54804a57268aaee57
Pavol Hejny <me@pavolhejny.com>
2021-06-07 20:28:55 +0200

    Update of modules SDK

--- src/CameraTool.tsx
+++ src/CameraTool.tsx
@@ -1,4 +1,11 @@
-import { declareModule, Icon, ImageArt, makeIconModuleOnModule, ToolbarName } from '@collboard/modules-sdk';
+import {
+    declareModule,
+    FocusScopeName,
+    Icon,
+    ImageArt,
+    makeIconModuleOnModule,
+    ToolbarName,
+} from '@collboard/modules-sdk';
 import * as React from 'react';
 import { Vector } from 'xyzt';
 import packageJson from '../package.json';
@@ -15,38 +22,40 @@ declareModule(() => {
             ...packageJson,
         },
         toolbar: ToolbarName.Tools,
-        icon: ({ apiClientPromise, materialArtVersioningSystemPromise }) => ({
-            autoSelect: true,
-            section: 2,
-            focusScope: 'camera',
-            char: '📷',
-            boardCursor: 'crosshair',
-            menu: () => (
-                <>
-                    <Icon
-                        char="📸"
-                        onClick={async () => {
-                            //const track = stream.getTracks()[0];
-                            //let imageCapture = new ImageCapture(track);
+        async icon(systems) {
+            const { apiClient, materialArtVersioningSystem } = await systems.request(
+                'apiClient',
+                'materialArtVersioningSystem',
+            );
+            return {
+                autoSelect: true,
+                section: 2,
+                focusScope: FocusScopeName.Social /* TODO: !!! null + option to name it by string */,
+                char: '📷',
+                boardCursor: 'crosshair',
+                menu: (
+                    <>
+                        <Icon
+                            char="📸"
+                            onClick={async () => {
+                                //const track = stream.getTracks()[0];
+                                //let imageCapture = new ImageCapture(track);
 
-                            const imageUrl = await (
-                                await apiClientPromise
-                            ).fileUpload(await cameraArt!.capture('image/jpeg'));
+                                const imageUrl = await apiClient.fileUpload(await cameraArt!.capture('image/jpeg'));
 
-                            const imageArt = new ImageArt(imageUrl, 'camera');
-                            imageArt.shift = cameraArt!.shift;
+                                const imageArt = new ImageArt(imageUrl, 'camera');
+                                imageArt.shift = cameraArt!.shift;
 
-                            (await materialArtVersioningSystemPromise)
-                                .createPrimaryOperation()
-                                .newArts(imageArt)
-                                .persist();
-                        }}
-                    />
-                </>
-            ),
-        }),
+                                materialArtVersioningSystem.createPrimaryOperation().newArts(imageArt).persist();
+                            }}
+                        />
+                    </>
+                ),
+            };
+        },
         moduleActivatedByIcon: {
-            setup: async ({ virtualArtVersioningSystemPromise }) => {
+            async setup(systems) {
+                const { virtualArtVersioningSystem } = await systems.request('virtualArtVersioningSystem');
                 if (navigator.mediaDevices.getUserMedia) {
                     stream = await navigator.mediaDevices.getUserMedia({ video: true });
                 }
@@ -59,7 +68,7 @@ declareModule(() => {
 
                 //await forTime(100);
                 // !!! alert(123);
-                const operation = (await virtualArtVersioningSystemPromise).createPrimaryOperation().newArts(cameraArt);
+                const operation = virtualArtVersioningSystem.createPrimaryOperation().newArts(cameraArt);
 
                 const videoSize = await cameraArt!.videoSize();
                 console.log(videoSize);
